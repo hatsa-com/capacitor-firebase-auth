a82dbbbf939267f450b352f69e2bc652e65ad23b
Vitaliy <vi.tas0987@gmail.com>
2020-09-24 17:37:04 +0300

    Fix an error when user can not sign in after installing a fresh app

--- ios/Plugin/Plugin/Handlers/AppleProviderHandler.swift
+++ ios/Plugin/Plugin/Handlers/AppleProviderHandler.swift
@@ -9,7 +9,7 @@ let authorizedUserIdKey = "appleAuthorizedUserIdKey"
 
 @available(iOS 13.0, *)
 class AppleProviderHandler: NSObject, ProviderHandler {
-    var call: CAPPluginCall?
+    var call: CAPPluginCall? = nil
     var plugin: CapacitorFirebaseAuth? = nil
     var rawNonce: String?
     var profileInfo: [String:Any] = [:]
@@ -33,7 +33,28 @@ class AppleProviderHandler: NSObject, ProviderHandler {
         authorizationController.performRequests()
     }
 
-    public func isAuthenticated() -> Bool { Auth.auth().currentUser != nil }
+    public func isAuthenticated() -> Bool {
+        // self.call is nil for the first time, because "isAuthenticated" will be called before "signIn".
+        // This way we force user to log in, so "signIn" is called and we have a fresh "call" & tokens.
+        guard let callbackId = self.call?.callbackId else {
+            print("[isAuthenticated] Ops, there is no saved callbackId")
+            return false
+        }
+
+        if
+                let _ = self.plugin?.bridge.getSavedCall(callbackId) {
+            print("[isAuthenticated] Ops, there is no saved call")
+            return false
+        }
+
+        if
+                let providerId = Auth.auth().currentUser?.providerData.first?.providerID,
+                providerId == "apple.com" {
+            return true
+        }
+
+        return false
+    }
 
     public func fillResult(data: PluginResultData) -> PluginResultData {
         var jsResult: PluginResultData = [:]
@@ -54,7 +75,7 @@ class AppleProviderHandler: NSObject, ProviderHandler {
 extension AppleProviderHandler: ASAuthorizationControllerDelegate {
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
-            self.call?.reject("Please, try again.")
+            self.plugin?.handleError(message: "Can not get appleIDCredential. Please, try again.")
 
             return
         }
@@ -64,18 +85,23 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate {
 
         // Retrieve the secure nonce generated during Apple sign in
         guard let currentNonce = rawNonce else {
-            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            self.plugin?.handleError(
+                    message: "Invalid state: A login callback was received, but no login request was sent."
+            )
+            return
         }
 
         // Retrieve Apple identity token
         guard let appleIDToken = appleIDCredential.identityToken else {
             print("Failed to fetch identity token")
+            self.plugin?.handleError(message: "Failed to fetch identity token")
             return
         }
 
         // Convert Apple identity token to string
         guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
             print("Failed to decode identity token")
+            self.plugin?.handleError(message: "Failed to decode identity token")
             return
         }
 
@@ -99,6 +125,6 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate {
     }
 
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        self.call?.reject(error.localizedDescription)
+        self.plugin?.handleError(message: error.localizedDescription)
     }
 }
